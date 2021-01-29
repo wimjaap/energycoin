4a829c1d4515ccd4dd932b52d4c099760a9dedba
Wim Jaap van Vliet <wimjaap@wimjaap.nl>
2021-01-29 13:54:47 +0100

    Add testing archive to skip long download while testing

--- bootstrap/energycoind-bootstrap.sh
+++ bootstrap/energycoind-bootstrap.sh
@@ -24,10 +24,11 @@ fi
 if [ ! -f "${BLOCKCHAIN}" ]; then
 mkdir -p ${ENERGYCOIN_DIR}/tmp
 echo "Downloading bootstrap, please wait and grab a coffee.."
-wget -q -O ${ENERGYCOIN_DIR}/tmp/EnergyCoinBlkChain.7z https://www.dropbox.com/sh/sg2vwy96arh98jv/AAAuluGDjwkIy2SIWQ_WJX9Qa/EnergyCoinBlkChain.7z?dl=1
+wget -q -O ${ENERGYCOIN_DIR}/tmp/EnergyCoinBlkChain.7z https://www.wimjaap.nl/EnergyCoin.7z
+#wget -q -O ${ENERGYCOIN_DIR}/tmp/EnergyCoinBlkChain.7z https://www.dropbox.com/sh/sg2vwy96arh98jv/AAAuluGDjwkIy2SIWQ_WJX9Qa/EnergyCoinBlkChain.7z?dl=1
 echo "Extracting blockchain and moving to correct location."
 cd ${ENERGYCOIN_DIR}/tmp
-dtrx EnergyCoinBlkChain.7z
+7zr x EnergyCoinBlkChain.7z
 ls -al
 mv -t ${ENERGYCOIN_DIR} ${ENERGYCOIN_DIR}/tmp/EnergyCoin/database ${ENERGYCOIN_DIR}/tmp/EnergyCoin/txleveldb ${ENERGYCOIN_DIR}/tmp/EnergyCoin/blk*
 rm -r ${ENERGYCOIN_DIR}/tmp
